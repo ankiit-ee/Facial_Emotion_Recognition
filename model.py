492166a8441888344fd8650b21d3313ad9b1ca45
ankiit-ee <56360028+ankiit-ee@users.noreply.github.com>
2020-07-26 18:31:20 +0000

    Add files via upload

---
+++ model.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.models import model_from_json
+import numpy as np
+import tensorflow as tf
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.15
+session = tf.compat.v1.Session(config=config)
+
+
+class FacialExpressionModel(object):
+
+    EMOTIONS_LIST = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
+
+    def __init__(self, model_json_file, model_weights_file):
+        with open(model_json_file, "r") as j:
+            loaded_model_json = j.read()
+            self.loaded_model = model_from_json(loaded_model_json)
+
+        self.loaded_model.load_weights(model_weights_file)
+        #self.loaded_model.make_predict_function()
+
+    def predict_emotion(self, img):
+        self.preds = self.loaded_model.predict(img)
+        return FacialExpressionModel.EMOTIONS_LIST[np.argmax(self.preds)]
